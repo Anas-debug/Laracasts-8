a596fd41f2bb3656573bfa1259f2cc756954724c
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-06-28 16:37:57 +0100

    fixed resource_path typo and cache typo

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,11 +9,11 @@ class Post
         base_path();
 
         // checks if the file exists
-        if( !file_exists($path = ressource_path("posts/{$slug}.html") ) ){
+        if( !file_exists($path = resource_path("posts/{$slug}.html") ) ){
             return redirect('/');
         }
 
         //cache
-        $post = cache->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
+        return cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
     }
 }
