400eb579b31e2fb599de57d3359b7219eaf9df4b
Anas Saoui <62400765+Anas-debug@users.noreply.github.com>
2022-06-28 16:22:00 +0100

    Added business logic to the Post Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,12 @@ class Post
 {
     public static function find($slug)
     {
+        // checks if the file exists
+        if(!file_exists($path = __DIR__."/../ressources/posts/{$slug}.html")){
+            return redirect('/');
+        }
 
+        //cache
+        $post = cache->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
     }
 }
